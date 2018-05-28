041feb721de1dbd8e4a7f8b01818c9ad14a1f077
masterabram <abramogol@gmail.com>
2018-05-28 08:18:20 +0300

    basic commit

---
+++ if.py
@@ -0,0 +1,11 @@
+
+
+#if else statement
+
+distance = 121
+
+if (distance == 100) :
+     print("The jouney was long")
+
+     :
+     
---
+++ print.py
@@ -0,0 +1,11 @@
+name = "Abraham"
+
+print(name)
+print (name[2:4])
+
+
+# section two
+
+x = 123
+
+print(float(x))
